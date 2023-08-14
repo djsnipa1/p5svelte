3a4ea34bb9614b44ad4c5616ea9c9d1d5dceb05f
CodeSandbox <support@codesandbox.io>
2023-08-14 18:03:29 +0000

    Sandbox snapshot commit

---
+++ src/components/Grid.svelte
@@ -0,0 +1,48 @@
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
+}
+
+function draw() {
+  background(220);
+  let size = 40; // size of the shapes
+  let offset = size / 2; // offset for the staggered rows
+
+  for (let x = 0; x < width; x += size) {
+    for (let y = 0; y < height; y += size) {
+      // check if the current row is even or odd
+      let evenRow = (y / size) % 2 === 0;
+      if (evenRow) {
+        // draw the shape at (x, y)
+        rect(x, y, size, size);
+      } else {
+        // draw the shape at (x + offset, y)
+        rect(x + offset, y, size, size);
+      }
+    }
+  }
+}
+function setup() {
+  createCanvas(400, 400);
+  noLoop();
+}
+
+function draw() {
+  background(220);
+  let size = 40; // size of the shapes
+  let offset = size / 2; // offset for the staggered rows
+
+  for (let x = 0; x < width; x += size) {
+    for (let y = 0; y < height; y += size) {
+      // check if the current row is even or odd
+      let evenRow = (y / size) % 2 === 0;
+      if (evenRow) {
+        // draw the shape at (x, y)
+        rect(x, y, size, size);
+      } else {
+        // draw the shape at (x + offset, y)
+        rect(x + offset, y, size, size);
+      }
+    }
+  }
+l
--- src/components/Psylo.svelte
+++ src/components/Psylo.svelte
@@ -15,23 +15,41 @@
           p.colorMode(p.RGB, 1);
           p.noStroke();
         };
+        
+function cosn(v) {
+  return p.cos(v * p.TWO_PI) * 0.5 + 0.5;
+}
 
-const radius = Math.sqrt(0.5);
+function invCosn(v) {
+  return 1 - cosn(v);
+}
+
+const dotSize = 0.05;
+const radius = Math.sqrt(0.5) + dotSize;
 const PHI = (1 + Math.sqrt(5)) / 2;
 
+
+let t;
+const frames = 1000;
+
         p.draw = () => {
+          t = p.fract(p.frameCount / frames);
+          // t = p.mouseX / p.width;
+          
           p.scale(p.width, p.height);
           p.background(0);
           p.fill(1);
           
-          const count = 200;
+          const count = 1000 * invCosn(t);
           for (let i = 0; i < count; i++) {
             const f = i / count;
           const a = i / PHI;
           const dist = f * radius;
           const x = 0.5 + p.cos(a * p.TWO_PI) * dist;
           const y = 0.5 + p.sin(a * p.TWO_PI) * dist;
-          const r = 0.01;
+          
+          const sig = cosn(f - t * 6);
+          const r = sig * f * dotSize;
           p.circle(x, y, r);
           }
         };
